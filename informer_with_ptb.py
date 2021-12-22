6469e2955de69846525c22fec0920e5845f9ddb7
Kent Nyi Lynn <kent@pop-os.localdomain>
2021-12-22 15:17:07 +0630

    refactor the inline keyboards and add a keyboard for strength

--- informer_with_ptb.py
+++ informer_with_ptb.py
@@ -10,7 +10,7 @@ from reply_keyboards import (
     check_point_keyboard,
     Others_keyboard,
 )
-import inline_kb
+from inline_kb import StrengthPicker, TimePicker
 from telegram import ReplyKeyboardMarkup, Update, ReplyKeyboardRemove
 from telegram.ext import (
     Updater,
@@ -29,20 +29,45 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-TOWN_ALPHABET, TOWNSHIP, LOCATION, TIME, CATEGORY, DESCRIPTION, SOURCE, CONFIRMATION = range(8)
-inline_for_time = inline_kb.TimePicker()
-
+TIME, TOWN_ALPHABET, TOWNSHIP, CATEGORY, CATEGORY1, PIN_LOCATION, TEXT_LOCATION, DESCRIPTION, STRENGTH, ATTACHMENT, SOURCE = range(11)
+inline_for_strength = StrengthPicker()
+inline_for_time = TimePicker()
 def start(update: Update, context: CallbackContext) -> int:
-    """Start the conversation and asks the user the start letter of township"""
+    """Start the conversation and asks the user about when it happened"""
     update.message.reply_text(
-        "You can start informing."
+        "Choose the time of the event", reply_markup=inline_for_time.keyboard,
+    )
+    return TIME
+
+def handle_time_callback(update: Update, context: CallbackContext) -> int:
+    """Handle callback for timepicker and ask the town alpha when submitted"""
+    global inline_for_time
+    query = update.callback_query
+    query.answer()
+    if query.data == "hour_inc":
+        inline_for_time.inc_hour()
+    elif query.data == "hour_dec":
+        inline_for_time.dec_hour()
+    elif query.data == "minute_inc":
+        inline_for_time.inc_minute()
+    elif query.data == "minute_dec":
+        inline_for_time.dec_minute()
+    elif query.data == "submit":
+        """Save the time and ask for category"""
+        query.delete_message()
+        query.bot.send_message(
+            query.from_user.id,
         " Choose the first letter of the township where it happened."
         "North Dagon, South Dagon, East Dagon begins with ဒ and North and South Okkalapa begins with ဥ",
-        reply_markup= township_first_consonant_keyboard
-        )      
-    return TOWN_ALPHABET
+        reply_markup=township_first_consonant_keyboard,
+        )
+        return TOWN_ALPHABET
+    query.edit_message_text(
+        "Press Ok after you have chosen", reply_markup=inline_for_time.keyboard
+    )
+    return TIME
 
-def town_alpha(update: Update, context: CallbackContext) -> int:
+def choose_township(update: Update, context: CallbackContext) -> int:
     # choose the township 
     kb_dict = township_keyboards_dict
     chosen_consonant = update.message.text
@@ -53,63 +78,73 @@ def town_alpha(update: Update, context: CallbackContext) -> int:
         )
     return TOWNSHIP 
 
-def ask_location(update: Update, context: CallbackContext) -> int:
-    """Save the Township and ask for location"""
-    update.message.reply_text(
-        "Send the location of the event, you can pin the location by pressing attachments icon and choose location"
-    )
-    return LOCATION
-
-def ask_time(update: Update, context: CallbackContext) -> int:
-    """Save the location and ask for time"""
-    update.message.reply_text(
-        "Send when the event occured", reply_markup=inline_for_time.keyboard,
-    )
-    return LOCATION
-
-def change_time(update: Update, context: CallbackContext) -> int:
-    global inline_for_time
-    query = update.callback_query
-    query.answer()
-    if query.data == "hour_inc":
-        inline_for_time.hour_face.text = str(int(inline_for_time.hour_face.text) + 1)
-    elif query.data == "hour_dec":
-        inline_for_time.hour_face.text = str(int(inline_for_time.hour_face.text) - 1)
-    elif query.data == "minute_inc":
-        inline_for_time.minute_face.text = str(int(inline_for_time.minute_face.text) + 1)
-    elif query.data == "minute_dec":
-        inline_for_time.minute_face.text = str(int(inline_for_time.minute_face.text) - 1)
-    elif query.data == "submit":
-        """Save the time and ask for category"""
-        query.delete_message()
-        query.bot.send_message(
-            query.from_user.id,
-            "Time is saved, now categorise the event",
-            category_keyboard, 
-        )
-        return CATEGORY
-    query.edit_message_text(
-        "Press Ok after you have chosen", reply_markup=inline_for_time.keyboard
-    )
-    return LOCATION
-
-
 def ask_category(update: Update, context: CallbackContext) -> int:
+    """Save township and ask category"""
     update.message.reply_text(
         "Categorise the event",
         reply_markup=category_keyboard, 
         )
     return CATEGORY
 
-def ask_description(update: Update, context: CallbackContext) -> int:
+def category1(update: Update, context: CallbackContext) -> int: 
+    if update.message.text == "Others":
+        update.message.reply_text(
+            "Choose the category, choose none of the above if you cannot categorize", reply_markup=Others_keyboard
+        )
+        return CATEGORY1
+    elif update.message.text == "*စစ်ဆေးခြင်း/ဖမ်းစီးခြင်း*":
+        update.message.reply_text(
+            "Choose the category,", reply_markup=check_point_keyboard,
+            parse_mode='Markdown',
+        )
+        return CATEGORY1
+    else:
+        """Save the category and ask town consonant"""
+        update.message.reply_text(
+            """Send when the event occured, Pick a time and press OK to submit""",
+            reply_markup=inline_for_time.keyboard
+        )
+        return PIN_LOCATION
+
+def save_category_pin_location(update: Update, context: CallbackContext) -> int:
     update.message.reply_text(
-        "Type the details of what you saw"
+        """Pin the location"""
+    )
+    return PIN_LOCATION
+
+def save_location_ask_text_location(update: Update, context: CallbackContext) -> int:
+    """save pin location ask for location in text"""
+    update.message.reply_text(
+        """Now send the location of the event in text, you can put here landmarks nearby or the floor of the apartment building
+        If you have nothing to add, press space and skip""",
+    )
+    return TEXT_LOCATION
+
+def save_text_location_ask_description(update: Update, context: CallbackContext) -> int:
+    """Save text location and ask for description"""
+    update.message.reply_text(
+        """Write the detailed description of what happened"""
     )
     return DESCRIPTION
 
-def ask_source(update: Update, conetxt: CallbackContext) -> int:
+def save_description_ask_strength(update: Update, context:CallbackContext) -> int:
+    """Save description and ask the number of forces"""
     update.message.reply_text(
-        "What is your source?"
+        """Choose the estimated number of forces you saw"""
+    )
+    return STRENGTH
+
+def save_strength_ask_attachment(update: Update, context:CallbackContext) -> int:
+    """Save strength and ask for attachment if any"""
+    update.message.reply_text(
+        """Would you like to add some attachment to this information?"""
+    )
+    return ATTACHMENT
+
+def save_attachment_ask_source(update: Update, context: CallbackContext) -> int:
+    """Save attachment and ask source"""
+    update.message.reply_text(
+        """What is the source?"""
     )
     return SOURCE
 
@@ -134,16 +169,16 @@ def main() -> None:
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
         states = {
-            TOWN_ALPHABET : [MessageHandler(Filters.text, town_alpha)],
-            TOWNSHIP : [MessageHandler(Filters.text, ask_location)],
-            LOCATION : [
-                MessageHandler(Filters.location, ask_time),
-                CallbackQueryHandler(change_time),
-            ],
-            TIME : [MessageHandler(Filters.text, ask_category)],
-            CATEGORY : [MessageHandler(Filters.text, ask_description)],
-            DESCRIPTION : [MessageHandler(Filters.text, ask_source)],
-            SOURCE: [MessageHandler(Filters.text, ask_confirmation)]
+            TIME : [CallbackQueryHandler(handle_time_callback)],
+            TOWN_ALPHABET : [MessageHandler(Filters.text, choose_township)],
+            TOWNSHIP : [MessageHandler(Filters.text, ask_category)],
+            CATEGORY : [MessageHandler(Filters.text, category1)],
+            CATEGORY1 : [MessageHandler(Filters.text, save_category_pin_location)],
+            PIN_LOCATION : [MessageHandler(Filters.location, save_location_ask_text_location)],
+            TEXT_LOCATION : [MessageHandler(Filters.text, save_text_location_ask_description)],
+            DESCRIPTION : [MessageHandler(Filters.text, save_description_ask_strength)],
+            STRENGTH : [MessageHandler(Filters.text,  save_strength_ask_attachment)],
+            ATTACHMENT : [MessageHandler(Filters.text, save_attachment_ask_source)],
         },
         fallbacks=[CommandHandler('cancel',cancel)],
     )
--- inline_kb.py
+++ inline_kb.py
@@ -1,3 +1,4 @@
+from typing import Text
 from telegram.ext import InlineQueryHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from datetime import datetime
@@ -8,13 +9,11 @@ class TimePicker:
     hour_dec_button = InlineKeyboardButton("⬇", callback_data="hour_dec")
     minute_dec_button = InlineKeyboardButton("⬇", callback_data="minute_dec")
     ok_button = InlineKeyboardButton("OK", callback_data="submit")
-    current_time = datetime.now()
-    hour = current_time.hour % 24
-    minute = current_time.minute % 60
-    hour_face = InlineKeyboardButton(str(hour), callback_data="hour")
-    minute_face = InlineKeyboardButton(str(minute), callback_data="minute")
     
     def __init__(self):
+        self.current_time = datetime.now()
+        self.hour_face = InlineKeyboardButton(str(self.current_time.hour), callback_data="hour")
+        self.minute_face = InlineKeyboardButton(str(self.self.current_time.minute), callback_data="minute")
         self.keyboard = InlineKeyboardMarkup(
             [
                 [self.hour_inc_button, self.minute_inc_button],
@@ -23,5 +22,64 @@ class TimePicker:
                 [self.ok_button]
             ]
         )
-class strengthPicker:
-    pass
+
+    def inc_hour(self):
+        self.hour_face.text = (int(self.hour_face.text) + 1) % 24
+    
+    def dec_hour(self):
+        self.hour_face.text = (int(self.hour_face.text) - 1) % 24
+
+    def inc_minute(self):
+        self.minute_face.text = (int(self.minute_face.text) + 1) % 60
+    
+    def dec_minute(self):
+        self.minute_face.text = (int(self.minute_face.text) - 1) % 60
+
+    
+
+class StrengthPicker:
+    small_vehicle_text_button = InlineKeyboardButton("စစ်/ရဲကားသေး", callback_data="text")
+    small_vehicle_number_button = InlineKeyboardButton("0", callback_data="display")
+    small_vehicle_inc_button = InlineKeyboardButton("⬆", callback_data="small_vehicle_inc")
+    small_vehicle_dec_button = InlineKeyboardButton("⬇", callback_data="small_vehicle_dec")
+    large_vehicle_text_button = InlineKeyboardButton("စစ်/ရဲကားကြီး", callback_data="text")
+    large_vehicle_number_button = InlineKeyboardButton("0", callback_data="display")
+    large_vehicle_inc_button = InlineKeyboardButton("⬆", callback_data="large_vehicle_inc")
+    large_vehicle_dec_button = InlineKeyboardButton("⬇", callback_data="large_vehicle_dec")
+    civ_vehicle_text_button = InlineKeyboardButton("အိမ်စီးကား", callback_data="text")
+    civ_vehicle_number_button = InlineKeyboardButton("0", callback_data="display")
+    civ_vehicle_inc_button = InlineKeyboardButton("⬆", callback_data="civ_vehicle_inc")
+    civ_vehicle_dec_button = InlineKeyboardButton("⬇", callback_data="civ_vehicle_dec")
+    motorbike_text_button = InlineKeyboardButton("ဆိုင်ကယ်", callback_data="text")
+    motorbike_number_button = InlineKeyboardButton("0", callback_data="display")
+    motorbike_inc_button = InlineKeyboardButton("⬆", callback_data="motorbike_inc")
+    motorbike_dec_button = InlineKeyboardButton("⬇", callback_data="motorbike_dec")
+    other_vehicle_text_button = InlineKeyboardButton("တခြားကား", callback_data="text")
+    other_vehicle_number_button = InlineKeyboardButton("0", callback_data="display")
+    other_vehicle_inc_button = InlineKeyboardButton("⬆", callback_data="other_vehicle_inc")
+    other_vehicle_dec_button = InlineKeyboardButton("⬇", callback_data="other_vehicle_dec")
+    uniform_text_button = InlineKeyboardButton("uniform", callback_data="text")
+    uniform_number_button = InlineKeyboardButton("0", callback_data="display")
+    uniform_inc_button = InlineKeyboardButton("⬆", callback_data="uniform_inc")
+    uniform_dec_button = InlineKeyboardButton("⬇", callback_data="uniform_dec")
+    plain_text_button = InlineKeyboardButton("အရပ်ဝတ်", callback_data="text")
+    plain_number_button = InlineKeyboardButton("0", callback_data="display")
+    plain_inc_button = InlineKeyboardButton("⬆", callback_data="plain_inc")
+    plain_dec_button = InlineKeyboardButton("⬇", callback_data="plain_dec")
+
+    def __init__(self) -> None:
+        self.vehicle_keyboard = InlineKeyboardMarkup(
+            [
+                [self.small_vehicle_text_button, self.large_vehicle_text_button, self.civ_vehicle_text_button, self.motorbike_text_button, self.other_vehicle_text_button],
+                [self.small_vehicle_inc_button, self.large_vehicle_inc_button, self.civ_vehicle_inc_button, self.motorbike_inc_button, self.other_vehicle_inc_button],
+                [self.small_vehicle_number_button, self.large_vehicle_number_button, self.civ_vehicle_number_button, self.motorbike_number_button, self.other_vehicle_number_button],
+                [self.small_vehicle_dec_button, self.large_vehicle_dec_button, self.civ_vehicle_dec_button, self.motorbike_dec_button, self.other_vehicle_dec_button],
+            ]
+        )
+        self.personnel_keyboard = InlineKeyboardMarkup(
+            [
+                [self.uniform_text_button, self.plain_text_button],
+                [self.uniform_inc_button, self.plain_inc_button],
+                [self.uniform_dec_button, self.plain_dec_button],
+            ]
+        )
